9c9e9b11982868703fae6915a05afe1ece3f9d5d
Val-E <30366552+Val-E@users.noreply.github.com>
2021-09-18 12:15:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -29,38 +29,39 @@ import pandas as pd
 
 from zipfile import ZipFile
 from warnings import simplefilter
+from re import findall
 
+FORMAT = "utf-8"
 
-# set template as working directory
-os.chdir("./template")
+# autofill options for remarks part
+REMARK_FILL_OPTIONS: dict = {
+    # warning levels
+    "1a": "Die Versetzung ist zurzeit gefährdet.\n",
+    "1b": "Die Versetzung ist zurzeit stark gefährdet.\n",
+    "1c": "Die Versetzung ist zurzeit ausgeschlossen.\n",
 
-# do not print FutureWarnings from numpy
-simplefilter(action='ignore', category=FutureWarning)
+    # probationary period
+    "2a": "<form_of_address> hat die Probezeit bestanden.\n",
+    "2b": "Die allgemeine Schulpflicht ist erfüllt.\n",
 
-# create and configure logger
-logging.basicConfig(filename="../log_file.log", format="%(levelname)s: %(asctime)s; %(message)s;", filemode="w")
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+    # repetition, resignation, skipping
+    "3a": "<form_of_address> hat die Berufsbildungsreife erworben.\n",
+    "3b": "Dieses Zeugnis ist der Berufsbildungsreife / der erweiterten Berufsbildungsreife gleichwertig.\n",
 
-# unzip document
-with ZipFile("../template.docx") as docx_template:
-    docx_template.extractall("./")
+    # dyslexia
+    "4a": "Aufgrund von festgestellten Lese- und Rechtschreibschwierigkeiten wurden "
+          "die Lese- und Rechtschreibleistungen nicht in vollem Umfang bewertet.\n",
 
-    # load template for document.xml
-    with open(file="./word/document.xml", mode="r", encoding="utf8") as document_template:
-        content: str = document_template.read()
+    # german
+    "5a": "<form_of_address> hat an Fördermaßnahmen zur Verbesserung der deutschen Sprachkenntnisse teilgenommen.\n",
 
-# attributes for certificate
-key_list: np.array = np.array([
-    "vorname", "familienname", "geburtsdatum", "geschlecht", "deutsch", "deutsch_allgemein",
-    "deutsch_schriftlich", "englisch", "franzoesisch", "ethik", "geografie", "geschichte",
-    "politische_bildung", "mathematik", "biologie", "chemie", "physik", "kunst",
-    "religion", "wpu1_name", "wpu1_note", "wpu2_name", "wpu2_note", "musik",
-    "sport", "angebote", "bemerkungen", "versaeumte_tage", "unentschuldigte_tage", "versaeumte_stunden",
-    "unentschuldigte_stunden", "verspaetungen", "klasse", "semester"
-])
+    # theology class
+    "6a": "<form_of_address> hat am Religionsunterricht der Evangelischen Kirche teilgenommen.\n"
+          "Der Träger kann eine eigene Teilnahmebescheinigung bzw. Beurteilung erteilen.\n"
+    # Feel free to add your templates!
+}
 
-fill_options: dict = {
+FILL_OPTIONS: dict = {
     # general
     "fill_subject": "……………………………………………*)",
     "first_semester": "Schulhalbjahr /S̶c̶h̶u̶l̶j̶a̶h̶r̶",
@@ -77,30 +78,31 @@ fill_options: dict = {
     "unpicked_box": "☐"
 }
 
-remark_fill_options: dict = {
-    # warning levels
-    "1a": "Die Versetzung ist zurzeit gefährdet.\n",
-    "1b": "Die Versetzung ist zurzeit stark gefährdet.\n",
-    "1c": "Die Versetzung ist zurzeit ausgeschlossen.\n",
+# set template as working directory
+os.chdir("./template")
 
-    # probationary period
-    "2a": "<form_of_address> hat die Probezeit bestanden.\n",
-    "2b": "Die allgemeine Schulpflicht ist erfüllt.\n",
+# do not print FutureWarnings from numpy
+simplefilter(action='ignore', category=FutureWarning)
 
-    # repetition, resignation, skipping
-    "3a": "<form_of_address> hat die Berufsbildungsreife erworben.\n",
-    "3b": "Dieses Zeugnis ist der Berufsbildungsreife / der erweiterten Berufsbildungsreife gleichwertig.\n",
+# create and configure logger
+logging.basicConfig(filename="../log_file.log",
+                    format="[%(levelname)s]\t[%(asctime)s]\t%(message)s",
+                    filemode="w")
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
-    # dyslexia
-    "4a": "Aufgrund von festgestellten Lese- und Rechtschreibschwierigkeiten wurden die Lese- und Rechtschreibleistungen nicht in vollem Umfang bewertet.\n",
+# unzip template document
+with ZipFile("../template.docx") as docx_template:
+    docx_template.extractall("./")
+    with open(file="./word/document.xml", mode="r", encoding=FORMAT) as document_template:
+        # get document.xml from template
+        TEMPLATE_CONTENT: str = document_template.read()
 
-    # german
-    "5a": "<form_of_address> hat an Fördermaßnahmen zur Verbesserung der deutschen Sprachkenntnisse teilgenommen.\n",
-
-    # theology class
-    "6a": "<form_of_address> hat am Religionsunterricht der Evangelischen Kirche teilgenommen.\n"
-          "Der Träger kann eine eigene Teilnahmebescheinigung bzw. Beurteilung erteilen.\n"
-}
+        # get attributes for certificate
+        keys = findall(r"{+[\w]+}", TEMPLATE_CONTENT)
+        KEY_LIST = [string[1:-1] for string in keys]
+        KEY_LIST.append("geschlecht")
+        KEY_LIST_LEN = len(KEY_LIST)
 
 
 def get_all_file_paths() -> np.array:
@@ -118,6 +120,7 @@ def get_all_file_paths() -> np.array:
 
 def get_csv_table_data() -> np.array:
     student_data: np.array = np.array([])
+
     # iterate through all files
     for root, directories, files in os.walk("../tables"):
         for csv in files:
@@ -128,16 +131,16 @@ def get_csv_table_data() -> np.array:
             data: dict = {}
             try:
                 # if the user instead decides to use a .csv
-                data = pd.read_csv(csv_path, dtype=str, encoding="utf8")
+                data = pd.read_csv(csv_path, dtype=str, encoding=FORMAT).to_dict(orient="list")
             except ValueError:
                 try:
                     # the application expects from user to use an .xls by default
-                    data = pd.read_excel(csv_path, dtype=str, encoding="utf8")
+                    data = pd.read_excel(csv_path, dtype=str, encoding=FORMAT).to_dict(orient="list")
                 except ValueError:
                     # if the file should not be read
                     pass
 
-            data = data.to_dict(orient="list")
+            # add path to dataset for debug message
             data["path"] = csv_path
 
             # add dictionary to array
@@ -149,41 +152,42 @@ def get_csv_table_data() -> np.array:
 def generate_docx(docx_file_paths: np.array, student: dict) -> None:
     religion: str = student["religion"]
     if (religion != "nan") and (religion != ""):
-        student["religion_label"] = "religion"
+        student["religion_label"] = "Religion"
 
     # modify values based on sex
     if student["geschlecht"] == "w":
-        student["pronomen"] = fill_options["female_pronounce"]
-        student["form_of_address"] = fill_options["female_form_of_address"]
+        student["pronomen"] = FILL_OPTIONS["female_pronounce"]
+        student["form_of_address"] = FILL_OPTIONS["female_form_of_address"]
     elif student["geschlecht"] == "m":
-        student["pronomen"] = fill_options["male_pronounce"]
-        student["form_of_address"] = fill_options["male_form_of_address"]
+        student["pronomen"] = FILL_OPTIONS["male_pronounce"]
+        student["form_of_address"] = FILL_OPTIONS["male_form_of_address"]
     else:
-        logging.error(msg=f"Schüler ID: {student['schueler_id']}; Falsch Geschlechtsangabe: {student['geschlecht']}")
+        logging.error(msg=f"[Schüler ID: {student['schueler_id']}]\t[Falsch Geschlechtsangabe: {student['geschlecht']}]")
         return None
 
     klasse: str = student["klasse"]
     semester: str = student["semester"]
     remarks: str = student["bemerkungen"]
+
     # modify values based on semester
     if semester == "1":
-        student["semester"] = fill_options["first_semester"]
-        student["kreuz1"] = fill_options["unpicked_box"]
-        student["kreuz2"] = fill_options["picked_box"]
-        student["kreuz3"] = fill_options["unpicked_box"]
-        student["kreuz4"] = fill_options["picked_box"]
+        student["semester"] = FILL_OPTIONS["first_semester"]
+        student["kreuz1"] = FILL_OPTIONS["unpicked_box"]
+        student["kreuz2"] = FILL_OPTIONS["picked_box"]
+        student["kreuz3"] = FILL_OPTIONS["unpicked_box"]
+        student["kreuz4"] = FILL_OPTIONS["picked_box"]
 
         student["jahr"] = f"{student['jahr'] }/{student['jahr'] + 1}"
 
         student["neue_jahrgangsstuffe"] = "/"
-        student["bestanden"] = fill_options["not"]
+        student["bestanden"] = FILL_OPTIONS["not"]
 
     elif semester == "2":
-        student["semester"] = fill_options["second_semester"]
-        student["kreuz1"] = fill_options["picked_box"]
-        student["kreuz2"] = fill_options["unpicked_box"]
-        student["kreuz3"] = fill_options["picked_box"]
-        student["kreuz4"] = fill_options["unpicked_box"]
+        student["semester"] = FILL_OPTIONS["second_semester"]
+        student["kreuz1"] = FILL_OPTIONS["picked_box"]
+        student["kreuz2"] = FILL_OPTIONS["unpicked_box"]
+        student["kreuz3"] = FILL_OPTIONS["picked_box"]
+        student["kreuz4"] = FILL_OPTIONS["unpicked_box"]
 
         student["jahr"] = f"{student['jahr'] - 1}/{student['jahr']}"
 
@@ -191,47 +195,27 @@ def generate_docx(docx_file_paths: np.array, student: dict) -> None:
 
         # modify certificate for the case the student fails
         if "<1c>" in remarks:
-            student["bestanden"] = fill_options["not"]
+            student["bestanden"] = FILL_OPTIONS["not"]
         else:
-            student["bestanden"] = fill_options["cross_out_not"]
+            student["bestanden"] = FILL_OPTIONS["cross_out_not"]
     else:
-        logging.error(msg=f"Schüler ID: {student['schueler_id']}; Falsche Semesterangabe: {student['semester']}")
+        logging.error(msg=f"[Schüler ID: {student['schueler_id']}]\t[Falsche Semesterangabe: {student['semester']}]")
         return None
 
     # insert text for keywords
-    for options in remark_fill_options.keys():
-        remarks = remarks.replace(f"<{options}>", remark_fill_options[options])
+    for option in REMARK_FILL_OPTIONS.keys():
+        remarks = remarks.replace(f"<{option}>", REMARK_FILL_OPTIONS[option])
 
     # insert correct form of address
     remarks = remarks.replace("<form_of_address>", student["form_of_address"])
     student["bemerkungen"] = remarks
 
     # write document.xml with student data
-    with open(file="./word/document.xml", mode="w", encoding="utf8") as document:
-        document.write(content.format(
-            vorname=student["vorname"],                       familienname=student["familienname"],
-            geburtsdatum=student["geburtsdatum"],             klasse=student["klasse"],
-            semester=student["semester"],                     jahr=student["jahr"],
-            deutsch=student["deutsch"],                       mathematik=student["mathematik"],
-            deutsch_allgemein=student["deutsch_allgemein"],   deutsch_schriftlich=student["deutsch_schriftlich"],
-            englisch=student["englisch"],                     biologie=student["biologie"],
-            franzoesisch=student["franzoesisch"],             chemie=student["chemie"],
-            physik=student["physik"],                         ethik=student["ethik"],
-            kunst=student["kunst"],                           geografie=student["geografie"],
-            musik=student["musik"],                           geschichte=student["geschichte"],
-            sport=student["sport"],                           politische_bildung=student["politische_bildung"],
-            wpu1_name=student["wpu1_name"],                   wpu1_note=student["wpu1_note"],
-            religion_label=student["religion_label"],         religion=student["religion"],
-            wpu2_name=student["wpu2_name"],                   wpu2_note=student["wpu2_note"],
-            angebote=student["angebote"],                     kreuz1=student["kreuz1"],
-            kreuz2=student["kreuz2"],                         kreuz3=student["kreuz3"],
-            kreuz4=student["kreuz4"],                         bemerkungen=student["bemerkungen"],
-            versaeumte_tage=student["versaeumte_tage"],       unentschuldigte_tage=student["unentschuldigte_tage"],
-            versaeumte_stunden=student["versaeumte_stunden"], unentschuldigte_stunden=student["unentschuldigte_stunden"],
-            verspaetungen=student["verspaetungen"],           pronomen=student["pronomen"],
-            bestanden=student["bestanden"],                   neue_jahrgangsstuffe=student["neue_jahrgangsstuffe"],
-            datum=student["datum"]
-        ))
+    with open(file="./word/document.xml", mode="w", encoding=FORMAT) as document:
+        document_content: str = TEMPLATE_CONTENT
+        for key in KEY_LIST:
+            document_content = document_content.replace("{" + key + "}", student[key])
+        document.write(document_content)
 
     # build docx
     docx_path: str = f"../certificate/" \
@@ -272,22 +256,22 @@ def main() -> None:
     logging.info(msg="Datensätze werden gelesen und Zeugnisse geschrieben.")
 
     # get date for certificate
-    datum: str = str(input("Bitte geben Sie das Datum für die Zeugnisse an: "))
-    year: int = int(datum[-4:])
+    date: str = str(input("Bitte geben Sie das Datum für die Zeugnisse an: "))
+    year: int = int(date[-4:])
 
     # generate dictionary with all attributes from all files for all students
     for student_id in student_id_list:
         student: dict = {
             "schueler_id": str(student_id),
-            "datum": datum,
+            "datum": date,
             "jahr": year,
 
             # default values
-            "religion_label": fill_options["fill_subject"],
+            "religion_label": FILL_OPTIONS["fill_subject"],
             "religion": "",
-            "wpu1_name": fill_options["fill_subject"],
+            "wpu1_name": FILL_OPTIONS["fill_subject"],
             "wpu1_note": "",
-            "wpu2_name": fill_options["fill_subject"],
+            "wpu2_name": FILL_OPTIONS["fill_subject"],
             "wpu2_note": "",
         }
 
@@ -298,28 +282,32 @@ def main() -> None:
                 for i in range(len(data["schueler_id"])):
                     if str(data["schueler_id"][i]) == student_id:
                         # iterate through all attributes
-                        for key in key_list:
-                            try:
-                                value: str = str(data[key][i])
-                                if value != "nan":
-                                    student[key] = value
-                                    logging.info(msg=f"Schüler ID: {student['schueler_id']}; "
-                                                     f"Attribut: {key}; "
-                                                     f"Wert: {value}; "
-                                                     f"Pfad: {data['path'][3:]}"
-                                                 )
-                            except KeyError:
-                                pass
+                        for key in KEY_LIST:
+                            if not (key in ["kreuz1", "kreuz2", "kreuz3", "kreuz4",
+                                            "pronomen", "bestanden", "neue_jahrgangsstuffe"]):
+                                try:
+                                    value: str = str(data[key][i])
+                                    if value != "nan":
+                                        student[key] = value
+                                        logging.info(msg=f"[Schüler ID: {student['schueler_id']}]\t"
+                                                         f"[Attribut: {key}]\t"
+                                                         f"[Wert: {value}]\t"
+                                                         f"[Pfad: {data['path'][3:]}]"
+                                                     )
+                                except KeyError:
+                                    pass
             except KeyError:
                 pass
 
         # checks whether all attributes are available
-        if len(student.keys()) == 38:
+        if KEY_LIST_LEN - len(student.keys()) == 6:
             generate_docx(docx_file_paths, student)
         else:
-            for key in key_list:
+            for key in KEY_LIST:
                 if not (key in student.keys()):
-                    logging.error(msg=f"Schüler ID: {student['schueler_id']}; Fehlender Attribut: {key}")
+                    if not (key in ("kreuz1", "kreuz2", "kreuz3", "kreuz4",
+                                    "pronomen", "bestanden", "neue_jahrgangsstuffe")):
+                        logging.error(msg=f"[Schüler ID: {student['schueler_id']}]\t[Fehlender Attribut: {key}]")
 
     # remove student data from document.xml
     with open(file="./word/document.xml", mode="w") as document:
